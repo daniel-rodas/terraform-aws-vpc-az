c3dd5d500483ddd79f5d83f03942a3bbfd8c6c6c
Daniel Rodas <daniel.rodas1@hotmail.com>
2023-11-27 19:36:52 -0800

    Update content source

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 # Example for loop in output
+# https://stackoverflow.com/questions/69498813/how-to-filter-aws-subnets-in-terraform
 output "ids2" {
   value = {
     for id, attributes in data.aws_subnet.vpcsubnet : id => attributes if attributes.available_ip_address_count > 20
